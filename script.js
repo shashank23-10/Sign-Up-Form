3f94d11cd60ab9aca5250dad6f2530036cbb6c2a
Shashank  Pandey <shashankpandey2001@gmail.com>
2023-01-14 08:21:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//Dark Mode Implementation
+(function () {
+  document.querySelector(".dark-mode-button").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
+})();
+//Dark Mode Implementation Ends
